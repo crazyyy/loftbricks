fa65b2e15c0c798420efd7a44aa2b09c3003b59d
crazyyy <crazyyy@gmail.com>
2016-11-15 11:20:59 +0200

    second block

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -902,4 +902,11 @@ $(document).ready(function() {
     useScrollbar: false,
     changeWhenAnim: false
   });
+  // triangle set width
+  var screenWidth = $('.home-second--title').width();
+  var borderWidth = screenWidth / 2 + 'px';
+  $('<style>.title-with-arrow::after,.home-second .home-second--title::after{border-left-width:'+ borderWidth+';border-right-width:'+borderWidth+';margin-left:-'+borderWidth+';}</style>').appendTo('header');
+
+
+
 });
